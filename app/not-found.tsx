c292425bc663f94880e54b60b60f7627fcf174b0
odiabliuk <o.diabliuk@gmail.com>
2023-06-29 15:55:08 +0300

    #010: additional fix to Not Found Page (404)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
 					<div className="mt-10 flex items-center justify-center gap-x-6">
 						<Link
 							href="/"
-							className="rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700"
+							className="rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700"
 						>
 							Go back home
 						</Link>
