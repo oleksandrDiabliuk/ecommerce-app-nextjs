98f8cef23512e58a805060c938ff43a0e9ddd206
odiabliuk <o.diabliuk@gmail.com>
2023-06-30 13:51:21 +0300

    #013: split loading skeleton to separate component

--- app/products/loading.tsx
+++ app/products/loading.tsx
@@ -1,22 +1,5 @@
+import LoadingSkeleton from "@/components/LoadingSkeleton/LoadingSkeleton";
+
 export default function Loading() {
-	const n = 8;
-
-	return (
-		<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-15 lg:max-w-7xl lg:px-8">
-			<div className="h-3 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mb-4 animate-pulse"></div>
-			<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-				{[...Array(n)].map((elem, index) => (
-					<div key={index} role="status" className="w-full p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-400">
-						<div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-400">
-							<svg className="w-12 h-12 text-gray-200 dark:text-gray-300 object-contain" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" /></svg>
-						</div>
-						<div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mb-4"></div>
-						<div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5"></div>
-
-						<span className="sr-only">Loading...</span>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+	return <LoadingSkeleton />
 }
